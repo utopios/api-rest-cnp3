5b7535ed6a853687cb133dcd990dd9c38707a89c
ihab abadi <ihab@utopios.net>
2022-02-21 11:21:50 +0100

    first spring boot projet

---
+++ formationapi/src/test/java/com/cnp/formationapi/FormationapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cnp.formationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormationapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
